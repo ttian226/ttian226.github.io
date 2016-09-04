d8327540b8bfeb525e279afb686a7f05b1e33998
wangxu <ttian226@gmail.com>
2016-09-04 23:17:39 +0800

    colourful

--- demo/ripple-click/js/touch.js
+++ demo/ripple-click/js/touch.js
@@ -6,10 +6,33 @@ $(function () {
   $('body').on('touchstart', touch);
 
   function touch(e) {
+
+    var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    function getColor() {
+      var cArr = [];
+      for (let i = 0; i < 6; i++) {
+        var v = getRandomInt(0, 15);
+        cArr[i] = arr[v];
+      }
+
+      var color = '#';
+      for (let i = 0; i < cArr.length; i++) {
+        color += cArr[i];
+      }
+      return color;
+    }
+
     $('.ripple').remove();
     var w = 100;
 
-    var html = '<span class="ripple"></span>';
+    var html = '<span class="ripple" style="background-color: ' + getColor() + '"></span>';
     $(this).append(html);
 
     var x = e.originalEvent.touches[0].pageX - w / 2;
